1137e989f56d2ad789b183dea8301d2a2100b5a5
marleysidapple <bsiddhartha25@gmail.com>
2017-05-29 10:15:24 +0800

    added folders

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<Text>This is app</Text>
+		);
+	}
+}
+
+export default App;
